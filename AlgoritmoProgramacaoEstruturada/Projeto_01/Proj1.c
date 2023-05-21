a4fe046082b216da997da849c9f8a2ee3cc1004a
Mateus Dutra Santos <127903319+DutraMateus@users.noreply.github.com>
2023-05-21 16:05:33 -0300

    Update_Incluir1000

--- AlgoritmoProgramacaoEstruturada/Projeto_01/Proj1.c
+++ AlgoritmoProgramacaoEstruturada/Projeto_01/Proj1.c
@@ -4,8 +4,8 @@
 #include<stdlib.h>
 
 void PreencherInformacoes(char NomeCompleto[][40], char email[][30], char sexo[][10], char endereco[][50], double *altura, int vacina[]){
-    for(int i=0 ; i<2 ; i++){
-        printf("\nNome: ");
+    for(int i=0 ; i<1000 ; i++){
+        printf("\nInforme o NOME do usuario: ");
         fgets(NomeCompleto[i], 40 ,stdin);
         fflush(stdin);
 
@@ -14,29 +14,29 @@ void PreencherInformacoes(char NomeCompleto[][40], char email[][30], char sexo[]
         }
 
         do{
-            printf("\nEmail: ");
+            printf("\nInforme o EMAIL do usuario: ");
             fgets(email[i], 30, stdin);
             fflush(stdin);
         } while (strchr(email[i], '@') == 0);
 
         do{
-            printf("\nSexo: ");
+            printf("\nInforme o SEXO do usuario: ");
             fgets(sexo[i], 10, stdin);
             fflush(stdin);
         } while(strchr(sexo[i], 'feminino') == 0 && strchr(sexo[i], 'masculino') == 0 && strchr(sexo[i], 'indiferente') == 0);
 
-        printf("\nEndereco: ");
+        printf("\nInforme o ENDERECO do usuario: ");
         fgets(endereco[i], 50, stdin);
         fflush(stdin);
 
         do{
-            printf("\nAltura(metros): ");
+            printf("\nInforme o ALTURA(metros) do usuario: ");
             scanf("%.2f", &altura[i]);
             fflush(stdin);
         } while (altura[i] < 1 && altura[i] > 2);
         
         do{
-            printf("\nVacinado(1 - sim) (0 - nao): ");
+            printf("\nInfome se o usuario foi ou nao VACINADO(1 - sim) (0 - nao): ");
             scanf("%d", &vacina[i]);
             fflush(stdin);
         } while (vacina[i], '1' == 0 && vacina[i], '0' == 0);
@@ -44,9 +44,9 @@ void PreencherInformacoes(char NomeCompleto[][40], char email[][30], char sexo[]
 }
 
 int main(){
-    char NomeCompleto[2][40], email[2][30], sexo[2][10], endereco[2][50];
-    int i, vacina[2]; 
-    double altura[2];
+    char NomeCompleto[1000][40], email[2][30], sexo[1000][10], endereco[1000][50];
+    int i, vacina[1000]; 
+    double altura[1000];
 
     PreencherInformacoes(NomeCompleto, email, sexo, endereco, altura, vacina);
     
